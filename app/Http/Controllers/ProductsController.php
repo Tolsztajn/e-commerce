2463ed749ebe81b34d979ef2e50f622bbf82d9ec
tamara <tamiol1@hotmail.com>
2017-12-17 01:24:35 -0300

    seguir a partir de relaciones base de datos

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -18,4 +18,30 @@ return view('products.index', $variables);
 public function create() {
 	return view('products.create');
 }
+public function store(Request $request) {
+    $rules = [
+        "name" => "required|unique:products",
+        "cost" => "required|numeric",
+        "profit_margin" => "required|numeric",
+        "category_id" => "required|numeric|between:1,3"
+    ];
+
+    $messages = [
+        "required" => "El :attribute es requerido!",
+        "unique" => "El :attribute tiene que ser único!",
+        "numeric" => "El :attribute tiene que ser numérico!",
+        "between" => "El :attribute tiene que estar entre :min y :max!"
+    ];
+
+    $request->validate($rules, $messages);
+
+    $producto = \App\Product::create([
+        'name' => $request->input('name'),
+        'cost' => $request->input('cost'),
+        'profit_margin' => $request->input('profit_margin'),
+        'category_id' => $request->input('category_id')
+    ]);
+
+    return redirect('/productos');
+}
 }
--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -8,28 +8,64 @@
 <body>
 	<div class="container">
 		<h1>Agregar Productos</h1>
-		<form class="col-md-5" action="/productos/agregar" method="post">
-			{{ csrf_field() }}
-			<div class="form-group">
-				<label for="name">Nombre</label>
-				<input type="text" name="name" id="name" value="" class="form-control">
-			</div>
-			<div class="form-group">
-				<label for="cost">Precio</label>
-				<input type="text" name="cost" id="cost" value="" class="form-control">
-			</div>
-			<div class="form-group">
-				<label for="profit_margin">Ganancias</label>
-				<input type="text" name="profit_margin" id="profit_margin" value="" class="form-control">
-			</div>
-            <div class="form-group">
-				<label for="category_id">Categoría</label>
-				<input type="text" name="category_id" id="category_id" value="" class="form-control">
-			</div>
-			<div class="form-group">
-				<button type="submit" name="button" class="btn btn-primary">Enviar</button>
-			</div>
-		</form>
+    <form class="col-md-5" action="/productos/agregar" method="post">
+  	{{ csrf_field() }}
+  	<div class="form-group">
+  	<label for="name">Nombre</label>
+  	<input type="text" name="name" id="name" value="{{old('name')}}" class="form-control">
+  		@if ($errors->has('name'))
+  			<div class="alert alert-danger">
+  				<ul>
+  					@foreach ($errors->get('name') as $error)
+  					    <li>{{ $error }}</li>
+  					@endforeach
+  				</ul>
+  			</div>
+  		@endif
+  	</div>
+  	<div class="form-group">
+  		<label for="cost">Costo</label>
+  		<input type="text" name="cost" id="cost" value="{{old('cost')}}" class="form-control">
+  		@if ($errors->has('cost'))
+  			<div class="alert alert-danger">
+  				<ul>
+  					@foreach ($errors->get('cost') as $error)
+  					    <li>{{ $error }}</li>
+  					@endforeach
+  				</ul>
+  			</div>
+  		@endif
+  	</div>
+  	<div class="form-group">
+  		<label for="profit_margin">Ganancias</label>
+  		<input type="text" name="profit_margin" id="profit_margin" value="{{old('profit_margin')}}" class="form-control">
+  		@if ($errors->has('profit_margin'))
+  			<div class="alert alert-danger">
+  				<ul>
+  					@foreach ($errors->get('profit_margin') as $error)
+  					    <li>{{ $error }}</li>
+  					@endforeach
+  				</ul>
+  			</div>
+  		@endif
+  	</div>
+      <div class="form-group">
+                  <label for="category_id">Categoría (1-3):</label>
+                  <input type="text" name="category_id" id="category_id" value="{{old('category_id')}}" class="form-control">
+                  @if ($errors->has('category_id'))
+                      <div class="alert alert-danger">
+                          <ul>
+                              @foreach ($errors->get('category_id') as $error)
+                                  <li>{{ $error }}</li>
+                              @endforeach
+                          </ul>
+                      </div>
+                  @endif
+              </div>
+  	<div class="form-group">
+  		<button type="submit" name="button" class="btn btn-primary">Enviar</button>
+  	</div>
+  </form>
 	</div>
 </body>
 </html>
